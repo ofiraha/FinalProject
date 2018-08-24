1464eb5a77e571d99759018b70c02d953d597ca0
hadarcoh <hadar.cohen.93@gmail.com>
2018-08-24 16:51:14 +0300

    adding a toreadlist button and add button + add dummy string dynamiclly

--- app/src/main/java/com/controllers/MainActivity.java
+++ app/src/main/java/com/controllers/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.dbObjects.Book;
+import com.dbHandler.Book;
 import com.google.android.gms.samples.vision.ocrreader.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
