69254e3f9f15a9614c135d503ed11eb51d533b07
hadarcoh <hadar.cohen.93@gmail.com>
2018-08-29 16:23:29 +0300

    add a book node

--- app/src/main/java/com/controllers/BookNode.java
+++ app/src/main/java/com/controllers/BookNode.java
@@ -1,4 +1,25 @@
 package com.controllers;
 
-public class BookNode {
+import com.dbHandler.*;
+
+public class BookNode extends Book
+{
+    private boolean isBookChecked;
+
+    public BookNode(boolean isBookChecked) {
+        this.isBookChecked = isBookChecked;
+    }
+
+    public BookNode(String name, String author, boolean isBookChecked) {
+        super(name, author);
+        this.isBookChecked = isBookChecked;
+    }
+
+    public boolean isBookChecked() {
+        return isBookChecked;
+    }
+
+    public void setBookChecked(boolean bookChecked) {
+        isBookChecked = bookChecked;
+    }
 }
