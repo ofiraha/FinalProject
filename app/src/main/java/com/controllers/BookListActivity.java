82d7a1db3c0f5109c3a188f82f4ab2fd1d1efe18
hadarcoh <hadar.cohen.93@gmail.com>
2018-08-30 16:36:05 +0300

    add capture image by clicking on add button

--- app/src/main/java/com/controllers/BookListActivity.java
+++ app/src/main/java/com/controllers/BookListActivity.java
@@ -1,18 +1,37 @@
 package com.controllers;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.samples.vision.ocrreader.R;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class BookListActivity extends Activity
 {
+    private static final int CAMERA_PHOTO = 111;
+    private Uri imageToUploadUri;
     ArrayAdapter<String> adapter;
     ArrayList<String> bookListForView;
     ArrayList<BookNode> bookList;
@@ -28,11 +47,24 @@ public class BookListActivity extends Activity
         ListView listView=(ListView)findViewById(R.id.bookListView);
         listView.setAdapter(adapter);
 
+    }
+
+    private void addListeners()
+    {
         Button addButton=(Button)findViewById(R.id.addBookToListButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                //open the camera and take a picture of the name of the book
+                captureCameraImage();
+/*
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
+*/
+
+                //TODO: IMPORTANT!!! THIS - TAKE THE TEXT AND ADD IT TO THE LIST
+                /*
                 //add to the book node the name and the author- take from the camera
                 //Todo: take the name from the camera
                 String name = "harry poter";
@@ -44,17 +76,110 @@ public class BookListActivity extends Activity
 
                 bookListForView.add(name);
                 // notify listview of data changed
-                adapter.notifyDataSetChanged();
+                adapter.notifyDataSetChanged();*/
             }
 
         });
 
-
+    }
+    private void captureCameraImage() {
+        Intent chooserIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File f = new File(Environment.getExternalStorageDirectory(), "POST_IMAGE.jpg");
+        chooserIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        imageToUploadUri = Uri.fromFile(f);
+        startActivityForResult(chooserIntent, CAMERA_PHOTO);
     }
 
-/*    public void addBookToList(View view)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_PHOTO && resultCode == Activity.RESULT_OK) {
+            if(imageToUploadUri != null){
+                Uri selectedImage = imageToUploadUri;
+                getContentResolver().notifyChange(selectedImage, null);
+               // Bitmap bitmap = getBitmap(imageToUploadUri.getPath());
+                //Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+               //TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+
+               //SparseArray<TextBlock> item = textRecognizer.detect(frame);
+            }
+            else{
+                Toast.makeText(this,"Error while capturing Image",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+  /*  private Bitmap getBitmap(String path) {
+
+        Uri uri = Uri.fromFile(new File(path));
+        InputStream in = null;
+        try {
+            final int IMAGE_MAX_SIZE = 1200000; // 1.2MP
+            ContentResolver c=getContentResolver();
+            in=c.openInputStream(uri);
+
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, null, o);
+            in.close();
+
+
+            int scale = 1;
+            while ((o.outWidth * o.outHeight) * (1 / Math.pow(scale, 2)) >
+                    IMAGE_MAX_SIZE) {
+                scale++;
+            }
+            Log.d("", "scale = " + scale + ", orig-width: " + o.outWidth + ", orig-height: " + o.outHeight);
+
+            Bitmap b = null;
+            in = getContentResolver().openInputStream(uri);
+            if (scale > 1) {
+                scale--;
+                // scale to max possible inSampleSize that still yields an image
+                // larger than target
+                o = new BitmapFactory.Options();
+                o.inSampleSize = scale;
+                b = BitmapFactory.decodeStream(in, null, o);
+
+                // resize to desired dimensions
+                int height = b.getHeight();
+                int width = b.getWidth();
+                Log.d("", "1th scale operation dimenions - width: " + width + ", height: " + height);
+
+                double y = Math.sqrt(IMAGE_MAX_SIZE
+                        / (((double) width) / height));
+                double x = (y / height) * width;
+
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(b, (int) x,
+                        (int) y, true);
+                b.recycle();
+                b = scaledBitmap;
+
+                System.gc();
+            } else {
+                b = BitmapFactory.decodeStream(in);
+            }
+            in.close();
+
+            Log.d("", "bitmap size - width: " + b.getWidth() + ", height: " +
+                    b.getHeight());
+            return b;
+        } catch (IOException e) {
+            Log.e("", e.getMessage(), e);
+            return null;
+        }
+    }*/
+
+   public void addBookToList(View view)
     {
-        //LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        addListeners();
+
+
+
+       /* //LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
        // View bookListView = inflater.inflate(R.layout.book_list, null);
         //the book list -TODO : CREATE THE LIST IN ANOTHER PLACE
         ArrayList<BookNode>  bookList= new ArrayList<BookNode>();
@@ -107,7 +232,7 @@ public class BookListActivity extends Activity
         bookListScrollView.addView(ll);
 
         // Display the view
-        setContentView(bookListView);
-    }*/
+        setContentView(bookListView);*/
+    }
 
 }
