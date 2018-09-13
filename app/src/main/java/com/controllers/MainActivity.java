413764a3dff03fba2d77792763ff22394c874fc1
hadarcoh <hadar.cohen.93@gmail.com>
2018-09-13 13:58:42 +0300

    add a message for camera permission in main activity

--- app/src/main/java/com/controllers/MainActivity.java
+++ app/src/main/java/com/controllers/MainActivity.java
@@ -1,7 +1,11 @@
 package com.controllers;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -12,6 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
     private DatabaseReference mDatabase;
 
     @Override
@@ -20,12 +25,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        checkCameraPermission();
+    }
+
+    public void checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
     }
 
     public void openToReadListActivity(View view)
     {
         Intent openToReadListActivity = new Intent(this, BookListActivity.class);
-       // openToReadListActivity.putExtra("KEY_TEXT", 5);
+        // openToReadListActivity.putExtra("KEY_TEXT", 5);
         startActivity(openToReadListActivity);
     }
 
@@ -33,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
     public void openOcrActivity(View view) {
 
 
-       Intent openOcrActivity = new Intent(this, OcrCaptureActivity.class);
-       //openOcrActivity.putExtra("KEY_TEXT", 5);
-       startActivity(openOcrActivity);
+        Intent openOcrActivity = new Intent(this, OcrCaptureActivity.class);
+        //openOcrActivity.putExtra("KEY_TEXT", 5);
+        startActivity(openOcrActivity);
     }
 }
