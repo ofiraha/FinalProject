a0f9405bd74229a44ae4085a84b2152c34ba6bc0
hadarcoh <hadar.cohen.93@gmail.com>
2018-09-13 19:43:32 +0300

    add phone authentication with firebase + login ,logout activities

---
+++ app/src/main/java/com/controllers/CountryData.java
@@ -0,0 +1,11 @@
+package com.controllers;
+
+public class CountryData
+{
+    public static final String[] countryNames={"Israel"
+    };
+
+    public static  final String[]countryAreaCodes={
+      "972"
+    };
+}
