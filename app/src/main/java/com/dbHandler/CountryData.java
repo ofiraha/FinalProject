261a629d6679abc71487058c33584cfb97c92e48
ofiraha <aha.ofir1@gmail.com>
2018-09-30 10:46:02 +0200

    add canceled files

---
+++ app/src/main/java/com/dbHandler/CountryData.java
@@ -0,0 +1,11 @@
+package com.dbHandler;
+
+public class CountryData
+{
+    public static final String[] countryNames={"Israel", "UK"
+    };
+
+    public static  final String[]countryAreaCodes={
+      "972", "44"
+    };
+}
