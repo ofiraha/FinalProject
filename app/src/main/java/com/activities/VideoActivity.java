72956a6f1b7e280754a1e77c100c07b3986accdd
ofiraha <aha.ofir1@gmail.com>
2018-10-01 16:45:04 +0200

    update main logo, add video logic and design (show when loading and replay option)

--- app/src/main/java/com/activities/VideoActivity.java
+++ app/src/main/java/com/activities/VideoActivity.java
@@ -1,12 +1,14 @@
 package com.activities;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 
 import com.google.android.gms.samples.vision.ocrreader.R;
@@ -15,7 +17,12 @@ public class VideoActivity extends AppCompatActivity {
 
     private WebView mWevView;
     private VideoView mVideoView;
-    private Button mBtnPlay, mBtnPause;
+    private Button mBtnPlay, mBtnReplay;
+    private ProgressBar mProgressBar;
+
+    private Uri mUri;
+    private boolean isOn = false;
+    private boolean isFirstTime = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,32 +31,73 @@ public class VideoActivity extends AppCompatActivity {
 
         mVideoView = (VideoView) findViewById(R.id.video_view);
         mBtnPlay = (Button)findViewById(R.id.play_btn);
-        mBtnPause = (Button)findViewById(R.id.pause_btn);
+        mBtnReplay = (Button) findViewById(R.id.replay_btn);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_circle);
 
         Intent intent = getIntent();
         String str = intent.getStringExtra("uri");
-        Uri uri = Uri.parse(str);
+        final Uri mUri = Uri.parse(str);
 
-        mVideoView.setVideoURI(uri);
+        mVideoView.setVideoURI(mUri);
         mVideoView.requestFocus();
 
-        mBtnPlay.setOnClickListener(new View.OnClickListener() {
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
-            public void onClick(View v) {
-                mVideoView.start();
+            public void onPrepared(MediaPlayer mp) {
+                mProgressBar.setVisibility(View.INVISIBLE);
             }
         });
-        mBtnPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mVideoView.pause();
-            }
-        });
-    }
 
-    public void playVideo(String urlId) {
-        String url = "bSMZknDI6bg";
-        mWevView.loadData("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" + url +"\" frameborder=\"0\" allowfullscreen></iframe>",
-                "text/html" , "utf-8");
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                isFirstTime = true;
+                mVideoView.stopPlayback();
+                mBtnPlay.setBackground(getResources().getDrawable(R.drawable.play));
+            }
+        });
+
+        mBtnPlay.setOnClickListener(new View.OnClickListener() {
+            final Uri uri = mUri;
+
+            @Override
+            public void onClick(View v) {
+                if(isOn){
+                    mVideoView.pause();
+                    mBtnPlay.setBackground(getResources().getDrawable(R.drawable.play));
+                } else {
+                    mBtnPlay.setBackground(getResources().getDrawable(R.drawable.pause));
+
+                    if(isFirstTime) {
+                        mVideoView.setVideoURI(uri);
+                        mVideoView.requestFocus();
+                        mVideoView.start();
+                        mProgressBar.setVisibility(View.VISIBLE);
+                        isFirstTime = false;
+                    }
+                    else {
+                        mVideoView.resume();
+                    }
+                }
+                isOn = !isOn;
+            }
+        });
+
+        mBtnReplay.setOnClickListener(new View.OnClickListener() {
+            final Uri uri = mUri;
+
+            @Override
+            public void onClick(View v) {
+                if(mVideoView != null) {
+                    mVideoView.stopPlayback();
+                    mBtnPlay.setBackground(getResources().getDrawable(R.drawable.pause));
+
+                    mVideoView.setVideoURI(uri);
+                    mVideoView.requestFocus();
+                    mVideoView.start();
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    isFirstTime = false;
+                }
+            }
+        });
     }
 }
